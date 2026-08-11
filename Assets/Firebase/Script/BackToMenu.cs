42d1194f3f57d040bb975aee74d0356f9ee5d63d
khanh-CR <105543160+khanh-CR@users.noreply.github.com>
2026-08-11 13:35:14 +0700

    log in
    log in

---
+++ Assets/Firebase/Script/BackToMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    // Tên scene menu chính
+    public string menuSceneName = "Main Menu";
+
+    // Hàm gọi khi bấm nút X
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+}
